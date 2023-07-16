84b042a71630ac61e30e75bc141bc2f45676bd7a
YusukeNaito08 <yusuke0208na@gmail.com>
2023-07-16 16:01:11 +0900

    add-navbar/fontAwsome

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Home from "./components/Home";
 import CreatePost from "./components/CreatePost";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
